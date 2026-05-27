1fbc7de3ccea4fca4d75d9847673bd68381ce84c
quyenha1225 <quyenha419@gmail.com>
2026-05-27 15:47:37 +0700

    fixx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import Template from "./template/Template";
 import ProductDetail from "./products/detail/ProductDetail";
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Landing from "./landing/Landing";
 import ProductList from "./products/ProductList";
 
 function App() {
   return (
     <Template>
-      <Switch>
-        <Route path="/products" exact>
-          <ProductList />
-        </Route>
-        <Route path="/products/:slug">
-          <ProductDetail />
-        </Route>
-        <Route path="/" exact>
-          <Landing />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/products/:slug" element={<ProductDetail />} />
+        <Route path="/" element={<Landing />} />
+      </Routes>
     </Template>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./index.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
@@ -13,11 +13,11 @@ import { HashRouter as Router } from "react-router-dom";
 
 library.add(fas, far, fab);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <Router>
+    <App />
+  </Router>
 );
